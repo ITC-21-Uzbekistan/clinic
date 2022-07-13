becc333c1328c96e075f5b58d1d87a1223343ed9
SirojiddinBakhadirov <105272105+SirojiddinBakhadirov@users.noreply.github.com>
2022-07-13 05:14:36 -0700

    ok

--- apps/employee/models.py
+++ apps/employee/models.py
@@ -1,7 +1,13 @@
+from django.db import models
+
 from utils.AbstractUser import AbstractUser
 
 
 class Employee(AbstractUser):
+    diploma = models.ImageField(upload_to='diplomas/', null=True, blank=True)
+    image = models.ImageField(upload_to='employees/', null=True, blank=True)
+    experience = models.PositiveIntegerField(null=True, blank=True)
+    room_number = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.firstname
