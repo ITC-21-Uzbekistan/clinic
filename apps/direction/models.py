6f2ac6b2030529156d616627219119d0ac612ca1
SirojiddinBakhadirov <105272105+SirojiddinBakhadirov@users.noreply.github.com>
2022-07-13 02:43:15 -0700

    direction doctor yozildi

--- apps/direction/models.py
+++ apps/direction/models.py
@@ -1,23 +1,8 @@
 from django.db import models
 
 
-class Direction(models.Model):
-    REGION_CHOICE = (
-        ('Tashkent', 'Tashkent'),
-        ('Tashkent region', 'Tashkent region'),
-        ('Andijan region', 'Andijan region'),
-        ('Bukhara region', 'Bukhara region'),
-        ('Fergana region', 'Fergana region'),
-        ('Jizzakh Region', 'Jizzakh Region'),
-        ('Xorazm Region', 'Xorazm Region'),
-        ('Namangan Region', 'Namangan Region'),
-        ('Navoiy Region', 'Navoiy Region'),
-        ('Qashqadaryo Region', 'Qashqadaryo Region'),
-        ('Samarkand Region', 'Samarkand Region'),
-        ('Sirdaryo Region', 'Sirdaryo Region'),
-        ('Surxondaryo Region', 'Surxondaryo Region'),
-    )
+class DirectionCategory(models.Model):
+    direction = models.CharField(max_length=100)
 
-    region = models.CharField(max_length=50, choices=REGION_CHOICE, null=True, blank=True)
-
-
+    class Meta:
+        db_table = 'direction'
--- apps/doctor/models.py
+++ apps/doctor/models.py
@@ -4,8 +4,28 @@ from utils.AbstractUser import AbstractUser
 
 
 class Doctor(AbstractUser):
+    REGION_CHOICE = (
+        ('Tashkent', 'Tashkent'),
+        ('Tashkent region', 'Tashkent region'),
+        ('Andijan region', 'Andijan region'),
+        ('Bukhara region', 'Bukhara region'),
+        ('Fergana region', 'Fergana region'),
+        ('Jizzakh Region', 'Jizzakh Region'),
+        ('Xorazm Region', 'Xorazm Region'),
+        ('Namangan Region', 'Namangan Region'),
+        ('Navoiy Region', 'Navoiy Region'),
+        ('Qashqadaryo Region', 'Qashqadaryo Region'),
+        ('Samarkand Region', 'Samarkand Region'),
+        ('Sirdaryo Region', 'Sirdaryo Region'),
+        ('Surxondaryo Region', 'Surxondaryo Region'),
+    )
+
+    region = models.CharField(max_length=50, choices=REGION_CHOICE, null=True, blank=True)
     diploma = models.ImageField(upload_to='diplomas/', null=True, blank=True)
     image = models.ImageField(upload_to='image/', null=True, blank=True)
     experience = models.PositiveIntegerField(null=True, blank=True)
     room_number = models.PositiveIntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'doctor'
 
