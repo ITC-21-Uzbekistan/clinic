bcec1b5620cab35bb474fe128b9d3755a3a370b9
SirojiddinBakhadirov <105272105+SirojiddinBakhadirov@users.noreply.github.com>
2022-07-13 02:32:16 -0700

    direction doctor yozildi

--- apps/direction/models.py
+++ apps/direction/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Direction(models.Model):
+    REGION_CHOICE = (
+        ('Tashkent', 'Tashkent'),
+        ('Tashkent region', 'Tashkent region'),
+        ('Andijan region', 'Andijan region'),
+        ('Bukhara region', 'Bukhara region'),
+        ('Fergana region', 'Fergana region'),
+        ('Jizzakh Region', 'Jizzakh Region'),
+        ('Xorazm Region', 'Xorazm Region'),
+        ('Namangan Region', 'Namangan Region'),
+        ('Navoiy Region', 'Navoiy Region'),
+        ('Qashqadaryo Region', 'Qashqadaryo Region'),
+        ('Samarkand Region', 'Samarkand Region'),
+        ('Sirdaryo Region', 'Sirdaryo Region'),
+        ('Surxondaryo Region', 'Surxondaryo Region'),
+    )
+
+    region = models.CharField(max_length=50, choices=REGION_CHOICE, null=True, blank=True)
+
+
--- apps/doctor/models.py
+++ apps/doctor/models.py
@@ -5,3 +5,7 @@ from utils.AbstractUser import AbstractUser
 
 class Doctor(AbstractUser):
     diploma = models.ImageField(upload_to='diplomas/', null=True, blank=True)
+    image = models.ImageField(upload_to='image/', null=True, blank=True)
+    experience = models.PositiveIntegerField(null=True, blank=True)
+    room_number = models.PositiveIntegerField(null=True, blank=True)
+
