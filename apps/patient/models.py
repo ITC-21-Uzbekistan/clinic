b5f81950576bf5eae5731083fd2ffd94bc4e501b
Famexan <89004245+Famexan@users.noreply.github.com>
2022-07-14 13:25:15 +0500

    patient model yozildi

--- apps/patient/models.py
+++ apps/patient/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
-from utils.AbstractUser import AbstractUser
+from auth_user.models import User
 
 
-class Patient(AbstractUser):
-    pass
+class Patient(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    potent_history = models.ImageField(null=True, blank=True)
+    analis_history = models.ImageField(null=True, blank=True)
+
+
 
     class Meta:
         db_table = 'patient'
---
+++ apps/patient/serializers/serializers.py
@@ -0,0 +1,2 @@
+from apps.patient.models import Patient
+from rest_framework import serializers
