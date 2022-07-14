5066154a39f40418e6ef2961cfec36f605185e72
Famexan <89004245+Famexan@users.noreply.github.com>
2022-07-14 17:05:22 +0500

    patient model yozildi

--- apps/patient/models.py
+++ apps/patient/models.py
@@ -4,12 +4,9 @@ from auth_user.models import User
 
 
 class Patient(models.Model):
-
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    potent_history = models.ImageField(null=True, blank=True)
-    analis_history = models.ImageField(null=True, blank=True)
-
-
+    patient_history = models.ImageField(null=True, blank=True)
+    analise_history = models.ImageField(null=True, blank=True)
 
     class Meta:
         db_table = 'patient'
